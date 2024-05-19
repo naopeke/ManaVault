95e4adf4f187c051589b3d60922b4219a6939daf
naopeke <naopeke@protonmail.com>
2024-05-19 16:24:44 +0200

    jasmine karma

--- src/app/services/web-storage.service.spec.ts
+++ src/app/services/web-storage.service.spec.ts
@@ -1,12 +1,16 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClient } from '@angular/common/http';
 import { WebStorageService } from './web-storage.service';
 
 describe('WebStorageService', () => {
   let service: WebStorageService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: HttpClient, useValue: jasmine.createSpyObj('HttpClient', ['get', 'put'])}
+      ]
+    });
     service = TestBed.inject(WebStorageService);
   });
 
