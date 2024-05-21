65b837d9f93359e015cb37230323a872c0331123
naopeke <naopeke@protonmail.com>
2024-05-21 12:09:48 +0200

    environment

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,16 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyDBD0AE0-52L-B81m4o1qGejhp-GDAZtFo",
+
+        authDomain: "manavault-49f6d.firebaseapp.com",
+      
+        projectId: "manavault-49f6d",
+      
+        storageBucket: "manavault-49f6d.appspot.com",
+      
+        messagingSenderId: "753692607545",
+      
+        appId: "1:753692607545:web:0443de4d3fefd2d88a79e8"
+    }
+};
