0304e7f5ef92e249abcc48e511a653f37ec8188d
naopeke <naopeke@protonmail.com>
2024-05-22 10:58:56 +0200

    login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,18 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule, FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 import { InputGroupModule } from 'primeng/inputgroup';
 import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
 import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { DividerModule } from 'primeng/divider';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+
 
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ButtonModule, FormsModule, InputGroupModule, InputGroupAddonModule, InputTextModule],
+  imports: [ButtonModule, FormsModule, ReactiveFormsModule, InputGroupModule, InputGroupAddonModule, InputTextModule, PasswordModule, DividerModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  fb = inject(FormBuilder);
+  http = inject(HttpClient);
+  router = inject(Router);
 
+  public formGroup!: FormGroup;
+  value: string | undefined;
+  errorMessage: string | null = null;
+
+  constructor(){
+    this.builForm();
+  }
+
+
+  private builForm(){
+    this.formGroup = this.fb.nonNullable.group({
+      email: ['', [Validators.required]],
+      password: ['', [Validators.required]]
+    });
+    
+  }
+
+
+  onSubmit(): void {
+
+  }
 }
--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  fb = inject(FormBuilder);
+  http = inject(HttpClient);
+  authService = inject(AuthService);
+  router = inject(Router);
 
+  form = this.fb.nonNullable.group({
+    username: ['', Validators.required],
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+  });
+
+  errorMessage: string | null = null;
+
+  onSubmit(): void{
+    const rawForm = this.form.getRawValue();
+    this.authService.register(rawForm.email, rawForm.username, rawForm.password)
+      .subscribe(
+        () => {
+          this.router.navigateByUrl('/login');
+        }
+      )
+  }
 }
